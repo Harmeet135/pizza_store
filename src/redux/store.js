b8bbab092c088caf50a9be4413fa47e4cfe39081
harmeet <harmeetchahal9891@gmail.com>
2022-03-01 23:51:18 +0530

    Updates

--- src/redux/store.js
+++
@@ -1,6 +0,0 @@
-import { createStore } from "redux";
-import reducers from "./reducers/indxx";
-import thunk from 'redux-thunk';
-import { applyMiddleware } from 'redux';
-
-export const store = createStore(reducers, {}, applyMiddleware(thunk))
